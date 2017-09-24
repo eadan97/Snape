f4a2b766ba52303efedcbc6d0c193f78c27721d9
israelhercam <israelhercam@hotmail.com>
2017-09-23 19:02:02 -0600

    Reservar Sala
    TODO: Hacer una funcion que vea si existe una sala con una reserva en esa hora.
    Validar calificacion estudiante, numero de reservas por dia, numero de reservas por semana.
    Crear reserva (Está hecha pero presenta un error a la hora de guardar los participantes) Además, es mejor si se separa en otra clase.
    Correo (El código no está en este commit pero lo tengo en otro proyecto, solo necesito que los participantes funcionen para unirlos).

---
+++ Proyecto 1.2/src/Controllers/CrearEstudiantesController.java
@@ -0,0 +1,96 @@
+package Controllers;
+
+import Models.Estudiante;
+import Models.Wrappers.Estudiantes;
+import Utils.Enums.Carrera;
+import Utils.Utils;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+/**
+ * Clase para controlar la ventana para crear estudiantres.
+ * @author Esteban Esquivel
+ * @author Israel Herrera
+ * @author Israel Padilla
+ */
+public class CrearEstudiantesController extends MenuBarController {
+
+    Estudiantes estudiantes=new Estudiantes();
+
+    public TableView<Estudiante> tblEstudiantes;
+    public TableColumn tbcCarnet;
+    public TableColumn tbcNombre;
+    public TableColumn tbcCarrera;
+    public TableColumn tbcCorreo;
+    public TableColumn tbcTelefono;
+    public TableColumn tbcCalificacion;
+    public TextField txtNombre;
+    public TextField txtCarnet;
+    public TextField txtCorreo;
+    public TextField txtTelefono;
+    public ComboBox cBoxCarrera;
+
+
+    public void initialize(){
+        //Se le dice a cada TableColumn que dato va a recibir de la lista
+        tbcCarnet.setCellValueFactory(new PropertyValueFactory<>("carnet"));
+        tbcNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        tbcCarrera.setCellValueFactory(new PropertyValueFactory<>("carrera"));
+        tbcCorreo.setCellValueFactory(new PropertyValueFactory<>("correo"));
+        tbcTelefono.setCellValueFactory(new PropertyValueFactory<>("telefono"));
+        tbcCalificacion.setCellValueFactory(new PropertyValueFactory<>("calificacion"));
+
+        //se refresca la lista
+        refrescarLista();
+
+        poblarCarreras();
+
+    }
+
+    /**
+     * Metodo para refrecar el TableView con los estudiantes
+     */
+    public void refrescarLista(){
+        ObservableList<Estudiante> list = FXCollections.observableArrayList(estudiantes.getLista());
+        tblEstudiantes.setItems(list);
+    }
+
+    public void agregarEstudiante(ActionEvent actionEvent) throws Exception {
+        if (!Utils.validarCorreo(txtCorreo.getText())
+                ||!Utils.validarNumero(txtTelefono.getText())
+                ||!Utils.validarNumero(txtCarnet.getText())
+                ||estudiantes.verificarCarnet(Integer.parseInt(txtCarnet.getText()))
+                ||cBoxCarrera.getValue()==null
+                ||txtNombre.getText().isEmpty()){
+            Utils.mostrarError("Error","Error en los datos ingresados","Revise los datos ingresados!");
+            return;
+        }
+
+        Estudiante nuevo = new Estudiante(txtNombre.getText(),Integer.parseInt(txtCarnet.getText()),cBoxCarrera.getValue().toString(),txtCorreo.getText(),Integer.parseInt(txtTelefono.getText()));
+
+        txtNombre.setText("");
+        txtCarnet.setText("");
+        txtCorreo.setText("");
+        txtTelefono.setText("");
+        cBoxCarrera.setValue(null);
+
+        estudiantes.add(nuevo);
+        estudiantes.saveInXML();
+        refrescarLista();
+    }
+
+
+    private void poblarCarreras(){
+        for (Carrera carrera: Carrera.values()) {
+            cBoxCarrera.getItems().add(carrera);
+        }
+    }
+
+
+}
---
+++ Proyecto 1.2/src/Models/Wrappers/Horarios.java
@@ -0,0 +1,75 @@
+package Models.Wrappers;
+
+import Models.Horario;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Clase de horarios.
+ * Esta clase sirve de contenedor de los horarios, ademas es necesaria para la JABX.
+ *
+ * @author Esteban Esquivel
+ * @author Israel Herrera
+ * @author Israel Padilla
+ */
+
+@XmlRootElement(name = "horarios")
+public class Horarios {
+
+    private static ArrayList<Horario> lista=new ArrayList<>();
+
+    @XmlElement(name = "horario")
+    public ArrayList<Horario> getLista() {
+        return lista;
+    }
+
+    public void setLista(ArrayList<Horario> lista) {
+        this.lista = lista;
+    }
+
+    //Terminan los Getters y Setters
+
+
+
+    /**
+     * Guarda en un XML la lista de horarios.
+     * @throws Exception
+     */
+    public void saveInXML() throws Exception {
+
+        JAXBContext contextObj = JAXBContext.newInstance(Horarios.class);
+
+        Marshaller marshallerObj = contextObj.createMarshaller();
+        marshallerObj.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+        marshallerObj.marshal(this, new File("horariosDB.xml"));
+
+    }
+
+    /**
+     * Carga de un XML la lista de horarios.
+     * @throws JAXBException
+     */
+    public void loadFromXML() throws Exception {
+        try{
+        File file = new File( "horariosDB.xml" );
+        JAXBContext jaxbContext = JAXBContext.newInstance( Horarios.class );
+
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+        Horarios estudiantes = (Horarios)jaxbUnmarshaller.unmarshal( file );
+        setLista(estudiantes.getLista());
+        }catch (Exception e){
+            saveInXML();
+        }
+
+    }
+
+}
